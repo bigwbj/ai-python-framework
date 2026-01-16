254c76d14941982790f9dfc06714017fd8ceff99
Wilhelm <wilhelm.birzle@gmx.de>
2026-01-16 14:35:58 +0100

    check that only currntly availible files and folders are check, the rest was prepared and commented out for further use

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,15 @@
 import os
 
 def test_project_structure():
-    # Check if .gitignore exists
+    # Check if .gitignore file exists
     assert os.path.isfile('.gitignore'), ".gitignore file is missing."
-
-    # Check if docs/requirements.txt exists
+    # Check if requirements.txt file exists
     assert os.path.isfile('requirements.txt'), "requirements.txt file is missing."
 
-    # check if folder requirements exists
+    # check if folder structure exists
     assert os.path.exists('requirements'), "requirements folder is missing."
-    assert os.path.exists('use_case'), "use_case folder is missing."
-    assert os.path.exists('unit_test'), "unit_test folder is missing."
-    assert os.path.exists('src'), "src folder is missing."
+    # assert os.path.exists('use_case'), "use_case folder is missing."
+    # assert os.path.exists('unit_test'), "unit_test folder is missing."
+    # assert os.path.exists('src'), "src folder is missing."
     assert os .path.exists('tests'), "tests folder is missing."
     assert os.path.exists('docs'), "docs folder is missing." 
